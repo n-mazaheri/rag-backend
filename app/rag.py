738e65f2db9fa5ef1ae3e60cef2bdfdcaf989c6f
Nasrin <na.mazaheri@yahoo.com>
2025-09-13 23:39:39 +0330

    fix(cache)

--- app/rag.py
+++ app/rag.py
@@ -18,6 +18,9 @@ from dotenv import load_dotenv
 load_dotenv()
 OPENAI_ROUTER_TOKEN=os.getenv("OPENROUTER")
 
+# Set a writable cache directory inside your container
+os.environ["TRANSFORMERS_CACHE"] = "/app/.cache"
+os.makedirs("/app/.cache", exist_ok=True)
 
 # Embeddings
 embeddings = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL)
